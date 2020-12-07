faffe4d23e0c40f6dd3a7c02f083e05ff8cc4dc5
Nicolas Hanout <nicolas.hanout@gmail.com>
2020-12-07 12:38:47 -0800

    react app

---
+++ api/server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import stockquotes
+
+app = Flask(__name__, static_folder='../static', static_url_path='/')
+
+
+@app.route('/')
+def index():
+	return app.send_static_file('index.html')
+
+@app.route('/ping')
+def ping():
+	return {'message': 'pong'}
+
+@app.route('/invest', methods=['POST'])
+def invest():
+	return {'test': 'data'}
