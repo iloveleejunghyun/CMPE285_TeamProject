efbca64e573436d85a86567c2cc25c003b9aac60
JingWang <jingwang9302@gmail.com>
2020-12-08 14:27:50 -0800

    Add StockManager for stock potfolio

--- backend/StrategySelection.py
+++ backend/StrategySelection.py
@@ -1,6 +1,6 @@
 stock_strategy = {
     "ethicalInvest": {"AAPL": 0.3, "ADBE": 0.2, "MSFT": 0.2, "NSRGY": 0.2, "TSLA": 0.1},
-    "growthInvest": {"EXEL": 0.3, "MB": 0.2, "NKE": 0.3, "SQ": 0.2},
+    "growthInvest": {"EXEL": 0.3, "NKE": 0.4, "SQ": 0.3},
     "indexInvest": {"VTI": 0.3, "IXUS": 0.3, "ILTB": 0.4},
     "qualityInvest": {"MA": 0.3, "TCEHY": 0.25, "TSM": 0.2, "V": 0.25},
     "valueInvest": {"IAG": 0.15, "LPX": 0.15, "SAFM": 0.1, "TWTR": 0.35, "ZG": 0.25}
